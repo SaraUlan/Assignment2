c51ffd4af5e3608e7c76897e055652f35b38bdd6
SaraUlan <220872@astanait.edu.kz>
2023-04-14 17:09:45 +0600

    add method in main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       MyArrayList arrList = new MyArrayList();
+       arrList.add();
     }
 }
--- src/main/java/org/example/MyArrayList.java
+++ src/main/java/org/example/MyArrayList.java
@@ -1,6 +1,22 @@
 package org.example;
 
 public class MyArrayList<T> {
-    private Object[] array;
+    private Object[] arr;
+    private int enoughSize = 10;
     private int size = 0;
+    public MyArrayList() { arr = new Object[enoughSize];}
+    public void add(T newItem){
+        if(size == enoughSize){
+            increaseArr();
+        }
+        arr[size++] = newItem;
+    }
+    private void increaseArr(){
+        enoughSize = (int) (1.5 * enoughSize);
+        Object[] arr2 = new Object[enoughSize];
+        for (int i = 0; i< size; i++){
+            arr2[i] = arr[i];
+        }
+    }
+
 }

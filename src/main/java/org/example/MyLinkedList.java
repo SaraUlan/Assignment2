bc6f127052be5dae057c36b8735583411c17ffdb
SaraUlan <220872@astanait.edu.kz>
2023-04-14 18:03:24 +0600

    Private inner class Node

--- src/main/java/org/example/MyLinkedList.java
+++ src/main/java/org/example/MyLinkedList.java
@@ -3,4 +3,14 @@ package org.example;
 import java.util.*;
 
 public abstract class MyLinkedList<E> implements List<E> {
+    private class Node{
+        E element;
+        Node next;
+        Node previous;
+        Node (E element, Node next, Node previous){
+            this.element = element;
+            this.next = next;
+            this.previous = previous;
+        }
+    }
 }

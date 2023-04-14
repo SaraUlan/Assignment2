e5ef1026567d4632fc609c4a06a210d50edcd0a2
SaraUlan <220872@astanait.edu.kz>
2023-04-14 17:53:07 +0600

    Test methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,12 @@ public class Main {
     public static void main(String[] args) {
        MyArrayList arrList = new MyArrayList();
        arrList.add(2);
+       arrList.add("Sezim");
+       arrList.add("Samat");
+       arrList.add(2007);
+       System.out.println(arrList.get(0));
+       System.out.println(arrList.get(1));
+       System.out.println(arrList.get(2));
+       System.out.println(arrList.get(3));
     }
 }
